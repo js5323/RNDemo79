16a8bc65e1ad3091fdeaa677c193ae5e6f86a69c
js5323 <js5323@hotmail.com>
2025-05-23 16:03:45 +0800

    add mpaas

--- android/app/src/main/java/com/rndemo79/MainApplication.kt
+++ android/app/src/main/java/com/rndemo79/MainApplication.kt
@@ -1,6 +1,7 @@
 package com.rndemo79
 
 import android.app.Application
+import android.util.Log
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
@@ -11,6 +12,9 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
+import com.mpaas.core.MP
+import com.mpaas.core.MPInitParam
+
 
 class MainApplication : Application(), ReactApplication {
 
@@ -34,11 +38,16 @@ class MainApplication : Application(), ReactApplication {
     get() = getDefaultReactHost(applicationContext, reactNativeHost)
 
   override fun onCreate() {
-    super.onCreate()
-    SoLoader.init(this, OpenSourceMergedSoMapping)
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      load()
-    }
+      super.onCreate()
+      SoLoader.init(this, OpenSourceMergedSoMapping)
+      if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+          // If you opted-in for the New Architecture, we load the native entry point for this app.
+          load()
+      }
+
+      MP.init(
+          this,
+          MPInitParam.obtain().setCallback { Log.d("TAG", "mPaaS Init finish") }
+      )
   }
 }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,19 +1,25 @@
-import {Button} from '@react-navigation/elements';
+import { Button } from '@react-navigation/elements';
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 type Props = {};
 
 const pwaPocUrl =
   'https://wwwsit.aia.com.hk/content/dam/hk/iChange/poc/pwa/index.html';
 
-export default function HomeScreen({}: Props) {
+export default function HomeScreen({ }: Props) {
+
+  const handleScan = () => {
+    console.log('scan');
+  }
+
   return (
     <View style={styles.container}>
-      <Button screen="WebviewScreen" params={{url: pwaPocUrl}}>
+      <Button screen="WebviewScreen" params={{ url: pwaPocUrl }}>
         PWA POC
       </Button>
       <Button screen="OpenLink">Open a link</Button>
+      <Button onPress={handleScan}>Open mpaas Scan</Button>
     </View>
   );
 }
